f5e0c3178d079236199ffab3ca334ff3d8e3713c
m1l1c4 <57289672+m1l1c4@users.noreply.github.com>
2019-11-06 18:49:15 +0100

    Add files via upload
    First commit

---
+++ psw-isa/src/main/java/tim31/pswisa/ClinicalCenterSystemApplication.java
@@ -0,0 +1,13 @@
+package tim31.pswisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalCenterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalCenterSystemApplication.class, args);
+	}
+
+}
