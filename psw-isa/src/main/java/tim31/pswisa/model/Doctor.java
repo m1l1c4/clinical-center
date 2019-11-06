dbdc2e332826ee7e83660d20f8f53cb15446c06b
milica <milica@WINDOWS-RBDRS6A>
2019-11-06 20:00:09 +0100

    novo

---
+++ psw-isa/src/main/java/tim31/pswisa/model/Doctor.java
@@ -0,0 +1,10 @@
+package tim31.pswisa.model;
+
+public class Doctor {
+
+	private int br;
+	private int cc;
+	private int gg;
+	private int fff;
+}
+
---
+++ psw-isa/src/main/java/tim31/pswisa/model/User.java
@@ -0,0 +1,6 @@
+package tim31.pswisa.model;
+
+public class User {
+
+	private String name;
+}
