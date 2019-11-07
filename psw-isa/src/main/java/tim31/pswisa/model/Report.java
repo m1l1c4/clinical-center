9e45b5750d048a3c8ba386c6067e2ad72ae7ab60
user <user@user-PC>
2019-11-07 17:20:17 +0100

    Napravljena klasa Report koja predstavlja izvjestaj koji se nalazi u kartonu pacijenta a koji pise doktor tokom pregleda.

---
+++ psw-isa/src/main/java/tim31/pswisa/model/Report.java
@@ -0,0 +1,36 @@
+package tim31.pswisa.model;
+
+import java.util.ArrayList;
+
+public class Report {
+
+	private String diagnosis;
+	private ArrayList<String>medicines;
+	
+	public Report() {
+		medicines = new ArrayList<String>();
+	}
+
+	public Report(String diagnosis) {
+		super();
+		this.diagnosis = diagnosis;
+	}
+
+	public String getDiagnosis() {
+		return diagnosis;
+	}
+
+	public void setDiagnosis(String diagnosis) {
+		this.diagnosis = diagnosis;
+	}
+
+	public ArrayList<String> getMedicines() {
+		return medicines;
+	}
+
+	public void setMedicines(ArrayList<String> medicines) {
+		this.medicines = medicines;
+	}
+	
+	
+}
