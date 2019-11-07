20167a63d203230ab4d495a39fad152153374f47
user <user@user-PC>
2019-11-07 17:10:48 +0100

    Napravljena klasa Absence koja predstavlja 'zahtjev' za godisnji odmor ili neko odsustvo, na osnovu ovih podataka salje se zahtjev administratoru za odobrenje.

---
+++ psw-isa/src/main/java/tim31/pswisa/model/Absence.java
@@ -0,0 +1,47 @@
+package tim31.pswisa.model;
+
+import java.util.Date;
+
+public class Absence {
+
+	private String email; // da bismo znali ko salje zahtjev
+	private Date start;
+	private int duration;
+	
+	public Absence() {
+		
+	}
+
+	public Absence(String email, Date start, int duration) {
+		super();
+		this.email = email;
+		this.start = start;
+		this.duration = duration;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	
+}
