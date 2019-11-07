041ef92a655e2cdb6acfae9d3d819e04cb264104
user <user@user-PC>
2019-11-07 16:53:33 +0100

    Napravljena klasa Recipe koja predstavlja recept koji je propisan od strane jednog ljekara i koj ije ovjerila neka medicinska sestra.

---
+++ psw-isa/src/main/java/tim31/pswisa/model/Recipe.java
@@ -0,0 +1,56 @@
+package tim31.pswisa.model;
+
+public class Recipe {
+
+	private Patient patient;
+	private MedicalWorker nurse; // medicinska sestra koja ga je ovjerila
+	private MedicalWorker doctor; // doktor koji ga je propisao
+	private String medicine; // lijek koje je propisan
+	
+	public Recipe() {
+		
+	}
+
+	public Recipe(Patient patient, MedicalWorker nurse, MedicalWorker doctor, String medicine) {
+		super();
+		this.patient = patient;
+		this.nurse = nurse;
+		this.doctor = doctor;
+		this.medicine = medicine;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public MedicalWorker getNurse() {
+		return nurse;
+	}
+
+	public void setNurse(MedicalWorker nurse) {
+		this.nurse = nurse;
+	}
+
+	public MedicalWorker getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(MedicalWorker doctor) {
+		this.doctor = doctor;
+	}
+
+	public String getMedicine() {
+		return medicine;
+	}
+
+	public void setMedicine(String medicine) {
+		this.medicine = medicine;
+	}
+	
+	
+	
+}
