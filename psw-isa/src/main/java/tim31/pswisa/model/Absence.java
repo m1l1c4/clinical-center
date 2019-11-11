0fd708412fa93fd4ba274af8f2ead7457186323a
user <user@user-PC>
2019-11-11 15:20:51 +0100

    Napravljene izmjene u templejtu za front-end.

--- psw-isa/src/main/java/tim31/pswisa/model/Absence.java
+++ psw-isa/src/main/java/tim31/pswisa/model/Absence.java
@@ -2,7 +2,19 @@ package tim31.pswisa.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Absence {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	private String email; // to know who sends request
 	private Date start;
