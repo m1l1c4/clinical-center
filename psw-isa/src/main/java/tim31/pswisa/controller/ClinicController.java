74c1ea27ca20776cf2967d7b732739f182f8cf3c
user <user@user-PC>
2019-11-18 17:50:31 +0100

    Napravljene klase AdministratorService, AdministratorRepository i AdministratorContoller za manipulisanje podacima vezanim za administratora klinike.
    Pored ovoga dodate klase UserRepository i UserServer radi lakseg
    pronalaska odgovarajuceg administratora.
    
    Implementirane metode za editovanje odredjenog administratora kao i za
    dobijanje zeljenog.

--- psw-isa/src/main/java/tim31/pswisa/controller/ClinicController.java
+++ psw-isa/src/main/java/tim31/pswisa/controller/ClinicController.java
@@ -1,5 +1,6 @@
 package tim31.pswisa.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
