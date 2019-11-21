7be73a22e27beb90914e1ad894e84d39afa2e265
user <user@user-PC>
2019-11-21 16:55:12 +0100

    Modifikovan model u klasama Absence, Checkup, Clinic i MedicalWorker.
    Dodate anotacije @JsonManagedReference i @JsonBackReference koje
    oznacavaju parent-a i child kako bi se adekvatno serijalizovi JSON
    objekti (rekurzija).
    
    Kreiran data-postgres SQL fajl u kome se nalaze inicijalni podaci za
    unos u bazu.

--- psw-isa/src/main/java/tim31/pswisa/model/Clinic.java
+++ psw-isa/src/main/java/tim31/pswisa/model/Clinic.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+
 @Entity
 public class Clinic {
 
