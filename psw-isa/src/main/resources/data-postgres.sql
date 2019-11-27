9d37482aec677e9fc87db86d00b3c1839fb6bd34
milica <milica@WINDOWS-RBDRS6A>
2019-11-27 22:39:10 +0100

    Implementirano dodavanje i brisanje stavki sifarnika.
    Sifarnik povezan sa bazom podataka. Dodate naredbe za ubacivanje stavki
    u bazu. Implementirane metode za dodavanje novih stavki u sifarnike
    lijekova i dijagnoza. Omoguceno brisanje postojecih.

--- psw-isa/src/main/resources/data-postgres.sql
+++ psw-isa/src/main/resources/data-postgres.sql
@@ -3,3 +3,7 @@ INSERT INTO clinic_administrator(clinic, user_id) VALUES ('Bolnica', 1);
 INSERT INTO korisnik(enabled, email, ime, password, prezime, type, active, first_login) VALUES (true,'draganamihajlovic55@yahoo.com', 'Dragana', 'sifra1','Mihajlovic', 'DOKTOR', true, false);
 INSERT INTO clinic( address, city, clinic_name, rating) VALUES ('Stepe Stepanovica', 'Foca', 'Univerzitetska bolnica', 10);
 INSERT INTO medical_worker(end_hr, phone, rating, start_hr, type_of_doctor, clinic_id, user_id) VALUES (15, 12345, 9, 8, 'UROLOG', 1, 2);
+
+INSERT INTO codebook(name, code, code_type) values ('prvi', '124', 'LIJEK');
+INSERT INTO codebook(name, code, code_type) values ('drugi', '345', 'LIJEK');
+INSERT INTO codebook(name, code, code_type) values ('treci', '567', 'DIJAGNOZA');
