d36c719406eaac81a0710e0047bbb894e6311224
Ivana <kovacevici404@gmail.com>
2019-12-05 16:48:52 +0100

    dodate metode za odgovarajuce get zahteve za klinike i tipove i metoda za pretragu klinika

---
+++ psw-isa/src/main/java/tim31/pswisa/repository/CheckUpTypeRepository.java
@@ -0,0 +1,13 @@
+package tim31.pswisa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tim31.pswisa.model.CheckUpType;
+
+public interface CheckUpTypeRepository extends JpaRepository<CheckUpType, Long> {
+	List<CheckUpType> findAll();
+	CheckUpType findByName(String name);
+	
+}
