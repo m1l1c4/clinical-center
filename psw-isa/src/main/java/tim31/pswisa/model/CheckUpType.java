8dc699ac07a20ff31e9c30b298a2c2c356debb20
Ivana <kovacevici404@gmail.com>
2019-12-05 16:48:52 +0100

    dodate metode za odgovarajuce get zahteve za klinike i tipove i metoda za pretragu klinika

---
+++ psw-isa/src/main/java/tim31/pswisa/model/CheckUpType.java
@@ -0,0 +1,12 @@
+package tim31.pswisa.model;
+
+import java.util.List;
+
+public class CheckUpType {
+
+	public List<Clinic> getClinics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
