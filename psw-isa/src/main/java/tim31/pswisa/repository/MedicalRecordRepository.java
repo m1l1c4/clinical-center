dad42f0eda836c0e6fc1416b82ae3b09d3e020a6
Ivana <kovacevici404@gmail.com>
2019-12-11 22:48:13 +0100

    reseni konflikti

--- psw-isa/src/main/java/tim31/pswisa/repository/MedicalRecordRepository.java
+++ psw-isa/src/main/java/tim31/pswisa/repository/MedicalRecordRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package tim31.pswisa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,14 +8,4 @@ import tim31.pswisa.model.MedicalRecord;
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
 
 }
-=======
-package tim31.pswisa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tim31.pswisa.model.MedicalRecord;
-
-public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
-
-}
->>>>>>> a720b0b87471e5f5257aec5d80ddf88830a376dc
--- psw-isa/src/main/java/tim31/pswisa/repository/ReportRepository.java
+++ psw-isa/src/main/java/tim31/pswisa/repository/ReportRepository.java
@@ -8,14 +8,5 @@ import tim31.pswisa.model.Report;
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
 }
-=======
-package tim31.pswisa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tim31.pswisa.model.Report;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-
-}
 
